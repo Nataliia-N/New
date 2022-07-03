bb2925cdc7b36f223c1f20430f959742ec0850ea
Nata <nata6nosenok@gmail.com>
2022-07-03 22:18:06 +0300

    link

--- src/js.js
+++ src/js.js
@@ -23,37 +23,53 @@ function currentDate(date) {
 }
 let time = document.querySelector("h2");
 time.innerHTML = currentDate();
+//
+function citynew(event) {
+    event.preventDefault();
+    let newCity = document.querySelector("#city");
+    let cityForm = document.querySelector("#city-submit");
+    newCity.innerHTML = cityForm.value;
+  }
+  let form = document.querySelector("#city-form");
+  form.addEventListener("submit", citynew);
+//
+  
+    function displayWeatherCondition(response) {
+        document.querySelector("#city").innerHTML = response.data.name;
+        document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
+        document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+        document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
+        document.querySelector("#description").innerHTML = response.data.weather[0].main;
+    }
 
-function city(event) {
-  event.preventDefault();
-  let newCity = document.querySelector("h1");
-  let cityForm = document.querySelector("#city-submit");
-  newCity.innerHTML = cityForm.value;
-}
-let form = document.querySelector("#city-form");
-form.addEventListener("submit", city);
+    function searchLocation(position) {
+        let apiKey = "e9e196ee12c6e5ec0599f3bdf6ebd323";
+        let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+    
+        axios.get(apiUrl).then(displayWeatherCondition);
+    }
 
+    function getCurrentLocation(event) {
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(searchLocation);
+    }
 
-function displayWeatherCondition(response) {
-  document.querySelector("#city-submit").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
-}
+    let currentLocationButton = document.querySelector("#Current-button");
+    currentLocationButton.addEventListener("click", getCurrentLocation);
+  
 
-function searchCity(city) {
-  var apiKey = "e9e196ee12c6e5ec0599f3bdf6ebd323";
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=".concat(city, "&appid=").concat(apiKey, "&units=metric");
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
+  function searchCity(city) {
+    let apiKey = "e9e196ee12c6e5ec0599f3bdf6ebd323";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeatherCondition);
+  }
 
-function searchLocation(position) {
-  var apiKey = "e9e196ee12c6e5ec0599f3bdf6ebd323";
-  var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=".concat(position.coords.latitude, "&lon=").concat(position.coords.longitude, "&appid=").concat(apiKey, "&units=metric");
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    var city = document.querySelector("#city-submit").value;
+    searchCity(city);
+  }
+  
+  let searchForm = document.querySelector("#city-form");
+  searchForm.addEventListener("submit", handleSubmit);
+  searchCity("Chernihiv");
